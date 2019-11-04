668c9e1d5016827c2f604c258521287b39bdf861
zasapp <54247667+zasapp@users.noreply.github.com>
2019-11-04 16:01:46 -0500

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,22 @@
+from graph import genpoints
+import help
+from numpy import *
+
+helper = {'/intersect':'<equation1> <equation2> <min> <max>: Calculate intersection between two y= equations'}
+
+def intersect(args):
+	if len(args) < 4:
+		help.helper("intersect")
+		return None
+	eq1 = args[1]
+	eq2 = args[2]
+	rg = range(int(float(args[3])),int(float(args[4])))
+	pts1x,pts1y = genpoints(eq1,rg,'x')
+	pts2x,pts2y = genpoints(eq2,rg,'x')
+	out = [] 
+	for x in rg:
+		if pts1x[x] == pts2x[x] and pts1y[x] == pts2y[x]:
+			out.append((pts1x[x],pts1y[x]))
+	for x in out:
+		print("(%s)\n" % (x,))
+coms = {'/intersect':intersect}
